6dfa78729ba68440becc04e3a548ca21a9088ad6
ConnorCleveland2002 <connorcleveland2002@gmail.com>
2021-08-12 22:26:12 -0400

    Total functionality

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,37 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Contact() {
+  const [errorEmail, setErrorEmail] = useState("");
+  const [errorName, setErrorName] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const leaveEmail = (evt) => {
+      setErrorEmail("This field cannot be blank")
+  };
+  const leaveName = (evt) => {
+    if (evt.target.value.length <= 0) {
+      setErrorName("This field cannot be blank");
+    } else setErrorName("");
+  };
+  const leaveMessage = (evt) => {
+    if (evt.target.value.length <= 0) {
+      setErrorMessage("This field cannot be blank");
+    } else setErrorName("");
+  };
+
+  const enterEmail = (evt) => {
+    setErrorEmail("");
+  };
+  const enterName = (evt) => {
+    setErrorName("")
+  };
+  const enterMessage = (evt) => {
+    setErrorMessage("")
+  };
+
+  const emailValidation = (evt) => {
+    const regex = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+    if (regex.test(evt.target.value)) {
+      setErrorEmail("Your email is valid.");
+    } else {
+      setErrorEmail("Your email in invalid.");
+    }
+  };
   return (
     <div id="contactme" className="contactme">
       <br></br>
       <h1>Contact Me!</h1>
       <hr></hr>
       <form action="">
-        <div onMouseLeave="leaveField()">
-          <label for="name">Name:</label>
+        <div>
+          <label>Name:</label>
           <br></br>
-          <input type="name" id="name" placeholder="Name"></input>
-          <div id="field" className="field"><p>Enter Name Above</p></div>
+          <input
+            type="name"
+            id="name"
+            placeholder="Name"
+            onBlur={leaveName}
+            onFocus={enterName}
+          ></input>
+          <span>{errorName}</span>
         </div>
         <hr></hr>
-        <div onMouseLeave="leaveField()">
-          <label for="email">Email:</label>
+        <div>
+          <label>Email:</label>
           <br></br>
-          <input type="email" id="email" placeholder="Email"></input>
-          <span type="email" id="emailSpan"></span>
+          <input
+            type="email"
+            id="email"
+            placeholder="Email"
+            onChange={emailValidation}
+            onBlur={leaveEmail}
+            onFocus={enterEmail}
+          ></input>
+          <span>{errorEmail}</span>
         </div>
         <hr></hr>
-        <div onMouseLeave="leaveField()">
-          <label for="message">Message:</label>
+        <div>
+          <label>Message:</label>
           <br></br>
           <textarea
             id="messasge"
             name="message"
             rows="10"
             cols="50"
-            required
+            onBlur={leaveMessage}
+            onFocus={enterMessage}
           ></textarea>
-          <span></span>
+          <span>{errorMessage}</span>
         </div>
         <br></br>
       </form>
--- src/components/pages/Works.js
+++ src/components/pages/Works.js
@@ -10,6 +10,7 @@ export default function Works() {
           <hr />
 
           <iframe
+            title="Fun-Finder Demo"
             className="main-iframe"
             src="https://connorcleveland2002.github.io/fun-finder/homePage.html"
             alt="Fun-Finder Demo"
@@ -47,6 +48,7 @@ export default function Works() {
           <hr />
 
           <iframe
+            title="Weather Dashboard Demo"
             src="https://connorcleveland2002.github.io/Weather-Dashboard/"
             alt="Weather Dashboard Demo"
           ></iframe>
@@ -80,6 +82,7 @@ export default function Works() {
 
           <hr />
           <iframe
+            title="Note-Taker Demo"
             src="https://eleven-note-takerinator.herokuapp.com/index.html"
             alt="Note-Taker Demo"
           ></iframe>
@@ -94,7 +97,9 @@ export default function Works() {
               add a new note with a title and contents, then being able to save
               on their local machine the notes that they took, as well as the
               ability to be able to delete any notes they had taken but didn't
-              need anymore.
+              need anymore. Perhaps ina future build, one might be able to
+              export these notes as text files, or save them to one's machine
+              for future use.
             </p>
             <li>
               Deployed Link:
@@ -109,7 +114,107 @@ export default function Works() {
               </a>
             </li>
           </ul>
-          {/* 3 more Eggzamples */}
+
+          <hr />
+          <img
+            alt="Password Generator-Inator"
+            src="PasswordGenerator-inator.png"
+            className="worksImg"
+          ></img>
+          <ul>
+            <a href="https://connorcleveland2002.github.io/Password-Generator-inator/">
+              <h3>Password Generator-inator</h3>
+            </a>
+            <p>
+              Password Generator-inator's goal was to create a button that
+              allows users to create a unique password based on constraints
+              outlined by themselves. This was a rather simple project that
+              demonstrates a button generating a string. Using this application,
+              one can generate their password for future uses, and perhaps in a
+              future build qwill be able to name and save their passwords to the
+              application.
+            </p>
+            <li>
+              Deployed Link:
+              <a href="https://connorcleveland2002.github.io/Password-Generator-inator/">
+                Password Generator-inator Deployed
+              </a>
+            </li>
+            <li>
+              Github Link:
+              <a href="https://github.com/ConnorCleveland2002/Password-Generator-inator">
+                Password Generator-inator Github
+              </a>
+            </li>
+          </ul>
+
+          <hr />
+          <img
+            alt="Handlebars Portfolio"
+            src="HandlebarsPortfolio.png"
+            className="worksImg"
+          ></img>
+          <ul>
+            <a href="https://portfolio-connor-ray-cleveland.herokuapp.com/">
+              <h3>Handlebars Portfolio</h3>
+            </a>
+            <p>
+              Handlebars Portfolio's task was to create an updated portfolio
+              with technologies such as handlebars, bootstrap, and others to
+              create an up-to-date looking and functioning portfolio. With this
+              updated portfolio, employers can see the current technologies I am
+              capable of, and have a taste of just a few of my skills I've
+              learnt through my career. In the future, this react portfolio will
+              serve a my personal website, but even later that may change into
+              more advanced technologies.
+            </p>
+            <li>
+              Deployed Link:
+              <a href="https://portfolio-connor-ray-cleveland.herokuapp.com/">
+                Handlebars Portfolio Deployed
+              </a>
+            </li>
+            <li>
+              Github Link:
+              <a href="https://github.com/ConnorCleveland2002/Portfolio-Homework">
+                Handlebars Portfolio Github
+              </a>
+            </li>
+          </ul>
+
+          <hr />
+          <img
+            alt="Daily Scheduler"
+            src="WorkdayScheduler.png"
+            className="worksImg"
+          ></img>
+          <ul>
+            <a href="https://connorcleveland2002.github.io/Daily-Scheduler/">
+              <h3>Daily Scheduler</h3>
+            </a>
+            <p>
+              Daily Scheduler's task was to create an application to schedule
+              certain events through the day, see what hour of the day it is,
+              and see which hours have passed. For this application, grey served
+              as hours passed, green are hours to come, and red was the current
+              hour. Users can save a note to any time slot in the day, and it
+              will change which one is active based on what time of the day it
+              is. This simple tracker was one of the first applications I'd ever
+              created, and it goes to show how far I've come from that point.
+            </p>
+            <li>
+              Deployed Link:
+              <a href="https://connorcleveland2002.github.io/Daily-Scheduler/">
+                Daily Scheduler Deployed
+              </a>
+            </li>
+            <li>
+              Github Link:
+              <a href="https://github.com/ConnorCleveland2002/Daily-Scheduler">
+                Daily Scheduler Github
+              </a>
+            </li>
+          </ul>
         </div>
         <hr />
       </article>
